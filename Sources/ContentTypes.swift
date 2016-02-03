e537fbac55e79bdd15023656480518507683cc9e
acb <acb@argent>
2016-02-03 00:37:56 +0000

    Static file middleware: Content type is now inferred from path

---
+++ Sources/ContentTypes.swift
@@ -0,0 +1,56 @@
+func contentTypeForName(name: String) -> String {
+	
+	let contentTypes: [String:String] = [
+		"ai" : "application/postscript",
+		"aif" : "audio/aiff",
+		"aifc" : "audio/aiff",
+		"aiff" : "audio/aiff",
+		"au" : "audio/x-au",
+		"avi" : "video/avi",
+		"bmp" : "image/bmp",
+		"bz2" : "application/x-bzip2",
+		"c" : "text/plain",
+		"c++" : "text/plain",
+		"cc" : "text/plain",
+		"cpp" : "text/x-c",
+		"css" : "text/css",
+		"cxx" : "text/plain",
+		"eps" : "application/postscript",
+		"gif" : "image/gif",
+		"gtar" : "application/x-gtar",
+		"gz" : "application/x-gzip",
+		"gzip" : "application/x-gzip",
+		"h" : "text/plain",
+		"hdf" : "application/x-hdf",
+		"hh" : "text/plain",
+		"htm" : "text/html",
+		"html" : "text/html",
+		"ico" : "image/x-icon",
+		"java" : "text/plain",
+		"jfif" : "image/jpeg",
+		"jpeg" : "image/jpeg",
+		"jpg" : "image/jpeg",
+		"js" : "application/x-javascript",
+		"m" : "text/plain",
+		"m3u" : "audio/x-mpequrl",
+		"mid" : "audio/midi",
+		"midi" : "audio/midi",
+		"mp2" : "audio/mpeg",
+		"mp3" : "audio/mpeg3",
+		"mpg" : "video/mpeg",
+		"png" : "image/png",
+		"ps" : "application/postscript",
+		"shtml" : "text/html",
+		"swift" : "text/plain",
+		"tgz" : "application/gnutar",
+		"tif" : "image/tiff",
+		"tiff" : "image/tiff",
+		"txt" : "text/plain",
+		"wav" : "audio/wav",
+		"zip" : "application/zip"
+	]
+
+	let suffix = name.characters.split(".").last.map { String($0).lowercaseString }
+
+	return suffix.flatMap { contentTypes[$0] } ?? "application/octet-stream"
+}
--- Sources/StaticFileRequestHandler.swift
+++ Sources/StaticFileRequestHandler.swift
@@ -11,7 +11,9 @@ public struct StaticFileRequestHandler : HTTPRequestHandler {
 	public func handleRequest(request: HTTPRequest) -> Future<HTTPResponse> {
 		if let remainder =  request.path.remainderFromPrefix(self.pathPrefix) {
 			let filePath = self.staticDir + remainder
-			if let response = HTTPResponse(code: HTTPResponse.Code.OK, headers:["Content-Type": "application/octet-stream"], filePath:filePath) {
+			let contentType = contentTypeForName(request.path)
+			print("contentTypeForName(\(request.path)) -> \(contentType)")
+			if let response = HTTPResponse(code: HTTPResponse.Code.OK, headers:["Content-Type": contentType], filePath:filePath) {
 				return Future(immediate: response)
 			} else {
 				return Future(immediate: HTTPResponse.NotFound(["Content-Type": "text/plain"], content:"Not found"))
